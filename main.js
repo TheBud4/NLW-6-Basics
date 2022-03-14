c23492030d3c092df8750fc24e8bc9bee4a1d47f
TheBud4 <murilopmr13@hotmail.com>
2022-03-14 13:50:35 -0300

    master

---
+++ main.js
@@ -0,0 +1,33 @@
+// Abre e fecha o menu
+
+const nav = document.querySelector('#header nav')
+const toggle = document.querySelectorAll('nav .toggle')
+
+for (const element of toggle) {
+  element.addEventListener('click', function () {
+    nav.classList.toggle('show')
+  })
+}
+
+//esconder itens do menu ao click
+
+const links = document.querySelectorAll('nav ul li a')
+
+for (const link of links) {
+  link.addEventListener('click', function () {
+    nav.classList.remove('show')
+  })
+}
+// mudar o header da pagina quando der scroll
+const reader = document.querySelector('#header')
+const navHeight = header.offsetHeight
+
+window.addEventListener('scroll', function () {
+  if (this.window.scrollY >= navHeight) {
+    //scroll maior que a altura do header
+    header.classList.add('scroll')
+  } else {
+    //menor que a altura do header
+    header.classList.remove('scroll')
+  }
+})
